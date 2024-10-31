f5536c42779e1b8071fcd1af8b42f186f544a78b
Elwis-hub <Miniouki52@gmail.com>
2024-10-31 17:15:23 -0300

    Logical operators

--- FUN.js
+++ FUN.js
@@ -1,1 +1,36 @@
-let call = "call";
+// Logical operators
+
+// (||) OR 
+// let hour = 9
+// let isWeekend = true;
+
+// if (hour < 10 || hour > 18 || isWeekend) {
+//     console.log( 'The office is closed.' )
+// }
+
+// let fistName = ('');
+// let lastName = ('');
+// let nickName = ('');
+
+// console.log(fistName || lastName || nickName || "Elwis")
+
+// (&&) AND
+// let age = 12;
+// let minute = 30;
+
+// if (hour == 12 && minute == 30) {
+//     console.log("It's 12:30");
+// }
+
+// let age = 56
+
+// if (age >= !14 && age <= !90) {
+//     console.log("It's bettwen 14 and 90")
+// }
+// let age = 10
+
+// if (!(age >= 14 && age <= 90 )) {
+//     console.log("bettwen")
+// }
+// else (console.log("outside"))
+
